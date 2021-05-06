0607307efa42d93454369722af2337ace5f138e7
suzhi82 <suzhi82@gmail.com>
2021-05-07 00:51:09 +1000

    added Tests and README.md

--- src/main/scala/com/abc/spark/AnonDemoTest.scala
+++ src/main/scala/com/abc/spark/AnonDemoTest.scala
@@ -4,41 +4,56 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 import org.scalatest.{BeforeAndAfter, FlatSpec}
 
-//引入scalatest建立一个单元测试类，混入特质BeforeAndAfter，在before和after中分别初始化sc和停止sc，
-//初始化SparkContext时只需将Master设置为local(local[N],N表示线程)即可，无需本地配置或搭建集群，
 
 class AnonDemoTest extends FlatSpec with BeforeAndAfter {
+  // SparkContext Ref can be used in all tests
   var sc: SparkContext = _
 
-  it should "test success 1" in {
-    //其中参数为rdd或者dataframe可以通过通过简单的手动构造即可
+  // Test 1
+  it should "return an empty array" in {
     val seq = Seq()
     val rdd: RDD[String] = sc.parallelize(seq)
     val anonRDD: RDD[String] = AnonDemo.anonymise(rdd)
-    anonRDD.collect().foreach(println)
+    val arr: Array[String] = anonRDD.collect()
+    assert(arr.length == 0)
   }
 
-  it should "test success 2" in {
-    //其中参数为rdd或者dataframe可以通过通过简单的手动构造即可
-    val seq = Seq("Lily,Chan,15 Hillside Drive Campbelltown SA 5078,1995-10-11")
+  // Test 2
+  it should "return a empty string" in {
+    val seq = Seq("aaa,bbb,ccc,ddd,eee")
     val rdd: RDD[String] = sc.parallelize(seq)
     val anonRDD: RDD[String] = AnonDemo.anonymise(rdd)
-    anonRDD.collect().foreach(println)
+    val arr: Array[String] = anonRDD.collect()
+    assert(arr(0).equals(""))
   }
 
-  //it should "test success 3" in {
-  //  //其中参数为rdd或者dataframe可以通过通过简单的手动构造即可
-  //  val rdd: RDD[String] = sc.textFile("E:\\temp\\data\\data.csv")
-  //  val anonRDD: RDD[String] = AnonDemo.anonymise(rdd)
-  //  anonRDD.saveAsTextFile("E:\\temp\\data\\output")
-  //}
+  // Test 3
+  it should "test success" in {
+    val seq = Seq(
+      "Lily,Chan,15 Hillside Drive Campbelltown SA 5078,1995-10-11",
+      "Cherry,Zhang,16 Hillside Drive Campbelltown SA 5078,2000-12-22"
+    )
+    val rdd: RDD[String] = sc.parallelize(seq)
+    val anonRDD: RDD[String] = AnonDemo.anonymise(rdd)
+    val arr: Array[String] = anonRDD.collect()
+    assert(arr(0).equals("********,********,********,1995-10-11"))
+    assert(arr(1).equals("********,********,********,2000-12-22"))
+  }
 
-  //这里before和after中分别进行sparkcontext的初始化和结束，如果是SQLContext也可以在这里面初始化
+  // Test 4
+  it should "write the anonymised data to specified directory" in {
+    val rdd: RDD[String] = sc.textFile("E:\\temp\\data\\data.csv")
+    val anonRDD: RDD[String] = AnonDemo.anonymise(rdd)
+    anonRDD.saveAsTextFile("E:\\temp\\data\\output")
+  }
+
+  // Create SparkContext before tests
   before {
     val conf: SparkConf = new SparkConf().setAppName("AnonDemoTest").setMaster("local[*]")
     sc = new SparkContext(conf)
   }
 
+  // Close SparkContext after tests
   after {
     if (sc != null) {
       sc.stop()
